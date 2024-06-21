6afa26c49a1407f70858413900a62c6361f6713c
RonanCraft <SuperRonanCraft@gmail.com>
2024-06-21 13:28:23 -0400

    type remove

--- src/CATEGORIES.ts
+++ src/CATEGORIES.ts
@@ -1,27 +1,18 @@
-export namespace PLUGIN {
-  export enum Type {
-    ECONOMY,
-    GAMEMODE,
-    UTILITY,
-    WORLD,
-    RESOURCE_PACK,
-    CHAT,
-    COSMETIC,
-    MISC,
-    PROXY,
-  }
+export enum CATEGORY_PLUGIN {
+  ECONOMY,
+  GAMEMODE,
+  UTILITY,
+  WORLD,
+  RESOURCE_PACK,
+  CHAT,
+  COSMETIC,
+  MISC,
+  PROXY,
 }
 
-export namespace SERVER {
-  export enum Type {
-    SURVIVAL,
-    CREATIVE,
-    PRISON,
-    FACTIONS,
-  }
+export enum CATEGORY_SERVER {
+  SURVIVAL,
+  CREATIVE,
+  PRISON,
+  FACTIONS,
 }
-
-export const CATEGORY = {
-  PLUGIN,
-  SERVER,
-};
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { POST_TYPE } from "./POST_TYPE";
 
 export { PERMISSION, POST_TYPE };
 
-export { CATEGORY } from "./CATEGORIES";
+export * from "./CATEGORIES";
 export * from "./PLUGINS";
