6f61d625ad89371cf76ff9c4c62d3096a70e65f5
RonanCraft <SuperRonanCraft@gmail.com>
2024-06-21 12:48:00 -0400

    export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { POST_TYPE } from "./POST_TYPE";
 
 export { PERMISSION, POST_TYPE };
 
-// export * from "./categories";
-// export * from "./plugins";
+export * from "./categories";
+export * from "./plugins";
