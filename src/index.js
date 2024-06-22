749afc92fd6aa241926fed51ce30ad6e529e4ea9
RonanCraft <SuperRonanCraft@gmail.com>
2024-06-21 22:31:45 -0400

    commonjs

--- src/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ...require("./PERMISSION"),
-  ...require("./POST_TYPE"),
-  ...require("./CATEGORY_SERVER"),
-  ...require("./CATEGORY_PLUGIN"),
-  ...require("./PLUGIN_VERSION"),
-  ...require("./PLUGIN_SERVER"),
-};
