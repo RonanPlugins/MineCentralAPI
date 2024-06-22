119dd1b049cde96c8c1dca57f3c9c35b9242c809
RonanCraft <SuperRonanCraft@gmail.com>
2024-06-21 22:29:53 -0400

    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ...require("./PERMISSION"),
+  ...require("./POST_TYPE"),
+  ...require("./CATEGORY_SERVER"),
+  ...require("./CATEGORY_PLUGIN"),
+  ...require("./PLUGIN_VERSION"),
+  ...require("./PLUGIN_SERVER"),
+};
